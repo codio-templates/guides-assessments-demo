5ba855b7671c9d8369d0bb5636f32da0eab39f0d
Ian Jobling <ijobling@applicationcraft.com>
2020-09-17 09:47:29 +0000

    updates

--- partial/partial-test.sh
+++ partial/partial-test.sh
@@ -2,8 +2,6 @@
 
 # partial/partial-test.sh
 
-
-
 # We change the directory on the linux box to the location of the student's file
 cd /home/codio/workspace/partial
 
@@ -14,25 +12,17 @@ javac Challenge.java
 # java would have output the error to std out
 if [ $? -ne 0 ]; then exit 1; fi
 
-
 # run the progarm and see what number comes back
 OUTPUT1=("$(java Challenge)")
-# if there was a runtime error 
-# then exit the test and show the captured output to the user 
-if [ $? -ne 0 ]; then exit 1; fi
-echo "$OUTPUT1"
 
 if [ $OUTPUT1 -gt 66 ]; then
-  curl -s "$CODIO_PARTIAL_POINTS_V2_URL" -d points=10 -d format=txt -d feedback="A grade" > /dev/null
+  curl -s "$CODIO_PARTIAL_POINTS_V2_URL" -d points=10 -d format=txt -d feedback="A grade: $OUTPUT1 points" > /dev/null
   exit 0
 fi 
 if [ $OUTPUT1 -gt 33 ]; then
-  curl -s "$CODIO_PARTIAL_POINTS_V2_URL" -d points=5 -d format=txt -d feedback="B grade"  > /dev/null
+  curl -s "$CODIO_PARTIAL_POINTS_V2_URL" -d points=5 -d format=txt -d feedback="B grade: $OUTPUT1 points"  > /dev/null
   exit 0
 fi
 
-curl -s "$CODIO_PARTIAL_POINTS_V2_URL" -d points=0 -d format=txt -d feedback="Fail" > /dev/null
-
-
+curl -s "$CODIO_PARTIAL_POINTS_V2_URL" -d points=0 -d format=txt -d feedback="Fail: $OUTPUT1 points" > /dev/null
 exit 1
-
